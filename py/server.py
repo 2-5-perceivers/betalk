2bbb770be11581e79814ba3266d766def9a9b85a
VictorINT <victor.s.florescu@gmail.com>
2021-09-18 20:35:36 +0300

    added some cool functions to server.py noice

---
+++ py/server.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 9090
+FORMAT = 'utf-8'
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(message):
+	for client in clients:
+		client.send(message);
+
+def handle(client):
+	while True:
+		try:
+			message = client.recv(1024)
+			print(f"{nicknames[clients.index(client)]} says {message}")
+			broadcast(message)
+		except:
+			index = clients.index(client)
+			nicknames.pop(index)
+			clients.remove(client)
+			break
+
+def receive():
+	while True:
+		client, address = server.accept()
+		print(f"Connected with {str(address)}!")
+
+		client.send("__RCV__".encode(FORMAT))
+		nickname = client.recv(1024)
+		nicknames.append(nickname)
+		clients.append(client)
+
+		print(f"Nickname of the client is {nickname}")
+		broadcast(f"{nickname} joined the chat\n".encode(FORMAT))
+		client.send("Connected to the server".encode(FORMAT))
+		thread  = threading.Thread(target=handle, args=(client,))
+		thread.start()
+
+print("Server running ... ")
+receive()
