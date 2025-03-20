1815b6284a71d0bfbe8f359a51b8ed5beead5c2f
Rares Vanca <raresvanca@gmail.com>
2025-03-20 14:02:53 +0200

    Re creating flutter client platforms code

--- client_flutter/android/app/src/main/kotlin/com/perceivers25/betalk/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.perceivers25.betalk
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ client_flutter/android/app/src/main/kotlin/org/perceivers25/client_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package org.perceivers25.betalk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
